b26084c85a54ea0823100582db6933037f53560f
Sam Mun <therealsam@berkeley.edu>
2022-02-22 22:24:32 -0800

    learned how to access Django models and display stuff

--- rxndatsite/rxndatapp/urls.py
+++ rxndatsite/rxndatapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('transformation/', views.TransformationListView.as_view(), name='transformation'),
+    path('transformation/add', views.addtfm, name='addtransformation'),
+    path('transformation/<int:pk>', views.conditions_by_transformation, name='cndbytfm'),
 ]
--- rxndatsite/rxndatapp/views.py
+++ rxndatsite/rxndatapp/views.py
@@ -1,5 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Transformation, Condition, Reaction
+
+from rxndatapp.forms import AddTransformation
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', None)
+
+class TransformationListView(generic.ListView):
+    model = Transformation
+
+def conditions_by_transformation(request, pk):
+    conditions_list = Condition.objects.filter(transformation__id = pk)
+
+    print(f'\n Logging \n {conditions_list}\n')
+
+    context = {
+        'conditions_by_transformation_list': conditions_list,
+    }
+    return render(request, 'rxndatapp/conditions_by_transformation.html', context)
+
+def addtfm(request):
+
+    if request.method == 'POST':
+        form = AddTransformation(request.POST)
+
+        if form.is_valid():
+            new_tfm = Transformation(**form.cleaned_data)
+            new_tfm.save()
+
+        return HttpResponseRedirect(reverse('addtransformation'))
+
+    else:
+        initial_form = {
+            'name': 'the reaction',
+            'reactant_smarts': 'C',
+            'product_smarts': 'C',
+        }
+        form = AddTransformation(initial=initial_form)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'rxndatapp/addtfm.html', context)
